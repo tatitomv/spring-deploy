eccb87bb059f372a2170fb4e3780a90d762b9783
tatitomv <lmontev@gmail.com>
2022-10-19 19:48:38 +0200

    probando

--- src/main/java/com/springdeploy/demo/controller/HelloController.java
+++ src/main/java/com/springdeploy/demo/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.springdeploy.demo.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,5 +14,10 @@ public class HelloController {
     @GetMapping("/hola")
     public String hola(){
         return "Hola Como estas?";
+    }
+
+    @GetMapping("/hola/{name}")
+    public String hola_name(@PathVariable String name){
+        return "Hola "+name+" Como estas?";
     }
 }
